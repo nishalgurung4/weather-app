a4c5df96b701ae7aac8479192efa2756dca0c47d
Nishal Gurung <nishal.gurung4@outlook.com>
2022-05-09 22:31:32 +0545

    enable artisan command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,21 +2,33 @@
 
 namespace App\Console;
 
+use App\Console\Commands\PurgeWeather;
+use App\Console\Commands\SyncWeather;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        SyncWeather::class,
+        PurgeWeather::class
+    ];
+
+    /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  Schedule  $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('weather:sync')->everySixHours();
-        $schedule->command('weather:purge')->monthly();
+        $schedule->command('weather:sync')->everyFiveMinutes();
+        $schedule->command('weather:purge')->everyMinute();
     }
 
     /**
